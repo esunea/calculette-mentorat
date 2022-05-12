b7a4ac58638ae103ff42472d5d1494e58e6e8350
paul moulard <paul.esusen@gmail.com>
2022-05-12 19:41:30 +0200

    la meilleure caltos

--- src/components/calculatrice.jsx
+++ src/components/calculatrice.jsx
@@ -1,36 +1,50 @@
 import React from "react";
+import Card from '@mui/material/Card';
 
 const Calculatrice = (props) => {
-  let {calculText, setCalculText} = React.useState("")
+  const [calculText, setCalculText] = React.useState("");
   // const buttonValues = ["7","8","9","+","4","5","6","-"]
   const buttonValues = [
     ["7", "8", "9", "+"],
     ["4", "5", "6", "-"],
     ["1", "2", "3", "*"],
-    ["0", ".", "=", "/"],
+    ["C", "0", "=", "/"],
   ];
   const buttonClicked = (value) => {
-    setCalculText( calculText + "value")
+    if (value === "=") {
+      setCalculText(eval(calculText));
+    } else if (value === "C") {
+      setCalculText("");
+    } else {
+      setCalculText(calculText + value);
+    }
   };
 
   return (
     <div>
-      <h1>Bonjour, {props.name}</h1>;
-
-      <p>{calculText}</p>
-      {buttonValues.map((elem) => {
-
-        return elem.map((elem2) => {
+      <h1>Bonjour, {props.name}</h1>
+      <Card sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
+        <p>{calculText}</p>
+        <CardContent>
+        {buttonValues.map((elem) => {
           return (
-            <button onClick={(event) => buttonClicked(elem2)}>
-              {elem2}
-            </button>
+            <div>
+              {" "}
+              {elem.map((elem2) => {
+                return (
+                  <button
+                    key="{elem2}"
+                    onClick={(event) => buttonClicked(elem2)}
+                  >
+                    {elem2}
+                  </button>
+                );
+              })}{" "}
+            </div>
           );
-        });
-        
-      })
-      }
-
+        })}
+        </CardContent>
+      </Card>
     </div>
   );
 };
